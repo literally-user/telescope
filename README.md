d245a1c024febcd6b7242d060a790dcc2766891b
Davlat Davydov <literally_user@hotmail.com>
2026-09-01 20:04:57 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# telescope
-Track your connections with people on Telegram with [conode](https://github.com/literally-user/conode)!
--- src/telescope/main.py
+++
@@ -1,8 +0,0 @@
-# --- Nothing to see here. See "dev" branch. ---
-
-def main():
-    print("Hello from telescope!")
-
-
-if __name__ == "__main__":
-    main()
